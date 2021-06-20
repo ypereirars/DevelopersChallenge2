f36838a7dd591d5078eb59805be4d1782fdc2542
Yuri Silveira <ypereirars@gmail.com>
2021-06-20 12:24:57 -0300

    feat: add context class

---
+++ src/XayahFinances/XayahFinances.Infra/Context/FinancesContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using XayahFinances.Domain.Entities;
+
+namespace XayahFinances.Infra.Context
+{
+    public class FinancesContext : DbContext
+    {
+        public FinancesContext() { }
+
+        public FinancesContext(DbContextOptions<FinancesContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<BankAccount> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
