db6871f528b467894edfd9323e7a1b5fb1547a25
Yuri Silveira <ypereirars@gmail.com>
2021-06-20 15:11:09 -0300

    feat: add queries for bank account

--- src/XayahFinances/XayahFinances.Infra/Repositories/BankAccountRepository.cs
+++ src/XayahFinances/XayahFinances.Infra/Repositories/BankAccountRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
 using XayahFinances.Domain.Entities;
 using XayahFinances.Domain.Interfaces.Repositories;
 
@@ -8,6 +10,63 @@ namespace XayahFinances.Infra.Repositories
     {
         public BankAccountRepository(IDbConnection connection) : base(connection)
         {
+        }
+
+        public override long Create(BankAccount entity)
+        {
+            var query = @"INSERT INTO bank_accounts 
+                            (bank_id, account_number, account_type)
+                        OUTPUT INSERTED.ID
+                        VALUES
+                            (@BankId, @AccountNumber, @AccountType)";
+
+            var id = Connection.QuerySingle<long>(query, entity);
+            entity.Id = id;
+
+            return id;
+        }
+
+        public override void Delete(long id)
+        {
+            Connection.Query(@"DELETE FROM bank_accounts WHERE id = @id", new { id = id });
+        }
+
+        public override BankAccount Get(long id)
+        {
+            var query = @"
+                SELECT
+                    BA.bank_id as BankId,
+                    BA.account_number as AccountNumber,
+                    BA.account_type as AccountType
+                FROM
+                    bank_accounts BA
+                WHERE BA.id = @id";
+
+            return Connection.QuerySingle<BankAccount>(query, param: new { id = id });
+        }
+
+        public override IEnumerable<BankAccount> Get()
+        {
+            var query = @"
+                SELECT
+                    BA.bank_id as BankId,
+                    BA.account_number as AccountNumber,
+                    BA.account_type as AccountType
+                FROM
+                    bank_accounts BA";
+
+            return Connection.Query<BankAccount>(query);
+        }
+
+        public override void Update(BankAccount entity)
+        {
+            var query = @"
+                UPDATE bank_accounts SET
+                    bank_id as @BankId,
+                    account_number as @AccountNumber,
+                    account_type as @AccountType";
+
+            Connection.Query(query);
         }
     }
 }
