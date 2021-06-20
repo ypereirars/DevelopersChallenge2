0c6689c587667bddf60bb2b1ce4e8eca359aec8c
Yuri Silveira <ypereirars@gmail.com>
2021-06-20 15:10:45 -0300

    fix: override base methods

--- src/XayahFinances/XayahFinances.Infra/Repositories/TransactionRepository.cs
+++ src/XayahFinances/XayahFinances.Infra/Repositories/TransactionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using XayahFinances.Domain.Entities;
 using XayahFinances.Domain.Interfaces.Repositories;
 
@@ -8,6 +9,31 @@ namespace XayahFinances.Infra.Repositories
     {
         public TransactionRepository(IDbConnection connection) : base(connection)
         {
+        }
+
+        public override long Create(Transaction entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Delete(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override Transaction Get(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEnumerable<Transaction> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update(Transaction entity)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
